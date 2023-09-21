8e8dbc8bb8363e005b5520f268968bd4c38f2019
Андрей <andreifarafonow@mail.ru>
2023-09-21 23:05:27 +0300

    Add files via upload

---
+++ Select.sql
@@ -0,0 +1,5 @@
+SELECT Products.Name AS "Product",
+       Categories.Name AS "Category"
+FROM Products
+LEFT JOIN ProductsCategories ON Products.Id = ProductsCategories.ProductID
+LEFT JOIN Categories ON ProductsCategories.CategoryId = Categories.Id;
